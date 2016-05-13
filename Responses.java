ddd626af93f39a33fcb784efc1b3707524d2b4df
Helmi Baraja <helmi.baraja@gmail.com>
2016-05-13 23:17:18 +0800

    Import java classes

---
+++ Responses.java
@@ -0,0 +1,21 @@
+package HBURLConnection;
+
+public class Responses {
+
+	private String responseString;
+	private int responseCode;
+	
+	public void setResponseString(String responseString) {
+		this.responseString = responseString;
+	}
+	public String getResponseString() {
+		return responseString;
+	}
+	public void setResponseCode(int responseCode) {
+		this.responseCode = responseCode;
+	}
+	public int getResponseCode() {
+		return responseCode;
+	}
+	
+}
